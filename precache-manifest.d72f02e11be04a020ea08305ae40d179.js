1a797a3b98742d3bd5ce2834ce95bb7de7dfb343
Bohemia420 <ksumeet40@gmail.com>
2020-06-01 01:46:29 +0530

    Updates

---
+++ precache-manifest.d72f02e11be04a020ea08305ae40d179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2baf72e30c8a392b97cea54bacc06039",
+    "url": "/khanna-me/index.html"
+  },
+  {
+    "revision": "1029009d51d3ce71c152",
+    "url": "/khanna-me/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2480d2c7bc2c89ac930c",
+    "url": "/khanna-me/static/css/main.63a8a666.chunk.css"
+  },
+  {
+    "revision": "1029009d51d3ce71c152",
+    "url": "/khanna-me/static/js/2.18dc92d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/khanna-me/static/js/2.18dc92d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2480d2c7bc2c89ac930c",
+    "url": "/khanna-me/static/js/main.59a97e05.chunk.js"
+  },
+  {
+    "revision": "2e940c61d974253a1937",
+    "url": "/khanna-me/static/js/runtime-main.96433710.js"
+  }
+]);
